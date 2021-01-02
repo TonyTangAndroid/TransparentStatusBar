89a8ea238d3eac44d9dea97e2010a7ef30e2fdc1
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-01-01 19:37:02 -0800

    Add DemoFragment that shows under status bar

---
+++ app/src/main/java/com/dvik/transparentstatusbar/DemoFragment.kt
@@ -0,0 +1,16 @@
+package com.dvik.transparentstatusbar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DemoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_ad, container, false)
+    }
+}
--- app/src/main/java/com/dvik/transparentstatusbar/MapsActivity.kt
+++ app/src/main/java/com/dvik/transparentstatusbar/MapsActivity.kt
@@ -1,20 +1,12 @@
 package com.dvik.transparentstatusbar
 
 import android.os.Bundle
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity() {
 
-    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,15 +17,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             findViewById<FloatingActionButton>(R.id.fab2).setMarginTop(insets.systemWindowInsetTop)
             insets.consumeSystemWindowInsets()
         }
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        val markerPosition = LatLng(-6.2485422, 106.7972844)
-        mMap.addMarker(MarkerOptions().position(markerPosition))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markerPosition, 17f))
-    }
+
 }
